99a4fbd634a2516b23e290c0faf48c54d9b140df
SplittyDev <splittydev@gmail.com>
2013-04-28 16:05:14 +0200

    Filesystem updates

--- WitchOS/HAL/HDDMountPoint.cs
+++ WitchOS/HAL/HDDMountPoint.cs
@@ -51,6 +51,79 @@ namespace WitchOS.HAL
             }
             partition.WriteBlock(1, 1, data);
             Out.printf("Header created!\n");
+            Out.printf("Writing test file...");
+            WriteFile(2, 1, "HelloWorld", "Hello from WitchFS!");
+        }
+
+        public void WriteFile(byte block, byte blocksize, string filename, string content)
+        {
+            byte[] data = new byte[512];
+
+            fixed (byte* ptr = data)
+            {
+                WitchFile* file = (WitchFile*)ptr;
+                file->magic = 0x7cc;
+                file->startblock = block;
+                file->blocksize = blocksize;
+                file->endblock = (byte)(block + blocksize);
+                for (int i = 0; i < filename.Length; i++)
+                {
+                    file->filename[i] = (byte)filename[i];
+                }
+                for (int i = 0; i < "file".Length; i++)
+                {
+                    file->extension[i] = (byte)"file"[i];
+                }
+                for (int i = 0; i < content.Length; i++)
+                {
+                    file->content[i] = (byte)content[i];
+                }
+            }
+
+            partition.WriteBlock(block, blocksize, data);
+        }
+
+        public void InitFiles()
+        {
+            byte firstblock = 1;
+            byte position = (byte)(firstblock + 1);
+            while (true)
+            {
+                byte[] data = new byte[512];
+                partition.ReadBlock(position, 1, data);
+                WitchFile* current;
+                fixed (byte* ptr = data)
+                {
+                    current = (WitchFile*)ptr;
+                    Kernel.MemCopy((uint)ptr * position, (uint)current, 0, (uint)sizeof(WitchFile));
+                }
+                if (current->magic == 0x7cc)
+                {
+                    WitchFile file = new WitchFile();
+                    file.magic = 0x7cc;
+                    file.startblock = current->startblock;
+                    file.blocksize = current->blocksize;
+                    file.endblock = current->endblock;
+                    string x = ByteToString(current->filename);
+                    StringToByte(file.filename, x);
+                    Out.printf("File found: %s", ByteToString(current->filename));
+                    string y = ByteToString(current->extension);
+                    StringToByte(file.extension, y);
+                    string z = ByteToString(current->content);
+                    StringToByte(file.content, z);
+                    Files.Add(file);
+                }
+                else break;
+                position++;
+            }
+        }
+
+        public void ListFiles()
+        {
+            for (int i = 0; i < Files.Count; i++)
+            {
+                Out.printf("File [%i]: %s", i, "Unknown Filename");
+            }
         }
 
         public void Init()
@@ -75,19 +148,35 @@ namespace WitchOS.HAL
                 Out.printf("The WitchOS header is valid\n");
             }
             Out.printf("Getting files...");
+            InitFiles();
+            Out.printf("Listing files...");
+            ListFiles();
         }
 
-        public void InitFiles()
+        public string ReadFile(string filename)
         {
-            byte[] firstblock = new byte[512];
-
-        }
-
-        // IMPLEMENT!!!
-        public byte[] ReadFile(string directory, string filename, string extension)
-        {
-            WitchFile file = new WitchFile();
-            return new byte[] { 0x0 };
+            /*
+            byte[] data = new byte[512];
+            partition.ReadBlock(block, blocksize, data);
+            WitchFile* file = (WitchFile*)Cosmos.Core.Heap.MemAlloc((uint)sizeof(WitchFile));
+            fixed (byte* ptr = data)
+            {
+                Kernel.MemCopy((uint)ptr, (uint)file, 0, (uint)sizeof(WitchFile));
+            }
+            string output = ByteToString(file->content);
+             * */
+            for (int i = 0; i < Files.Count; i++)
+            {
+                byte[] data = new byte[512];
+                fixed (byte* ptr = data)
+                {
+                    if (ByteToString(ptr) == filename)
+                    {
+                        
+                    }
+                }
+            }
+            return "ERROR";
         }
 
         private string ByteToString(byte* data)
@@ -104,6 +193,32 @@ namespace WitchOS.HAL
                 pos++;
             }
             return str;
+        }
+        private byte[] ByteToByte(byte* data)
+        {
+            int length = 0;
+            int pos = 0;
+            while (true)
+            {
+                byte b = data[pos];
+                if (b != 0)
+                    length++;
+                else
+                    break;
+                pos++;
+            }
+            byte[] output = new byte[length];
+            pos = 0;
+            while (true)
+            {
+                byte b = data[pos];
+                if (b != 0)
+                    output[pos] = b;
+                else
+                    break;
+                pos++;
+            }
+            return output;
         }
         private void StringToByte(byte* data, string str)
         {
@@ -140,19 +255,23 @@ namespace WitchOS.HAL
             public byte root;
         }
 
-        [StructLayout(LayoutKind.Explicit, Pack = 1)]
+        [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 512)]
         public struct WitchFile
         {
             [FieldOffset(0)]
-            public fixed byte filename[240];
-            [FieldOffset(240)]
-            public fixed byte extension[15];
-            [FieldOffset(255)]
+            public short magic;
+            [FieldOffset(2)]
+            public fixed byte filename[25];
+            [FieldOffset(27)]
+            public fixed byte extension[10];
+            [FieldOffset(37)]
             public byte startblock;
-            [FieldOffset(256)]
+            [FieldOffset(38)]
             public byte endblock;
-            [FieldOffset(257)]
+            [FieldOffset(39)]
             public byte blocksize;
+            [FieldOffset(40)]
+            public fixed byte content[472];
         }
     }
 }
